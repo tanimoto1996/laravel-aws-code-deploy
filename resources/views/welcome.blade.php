c1bdb8666e5b48de14512a5e4786da86e806b388
tanimoto <yumenikki2@gmail.com>
2025-02-10 15:13:43 +0900

    自動デプロイテスト

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,10 +94,10 @@
                                         </div>
 
                                         <div class="pt-3 sm:pt-5 lg:pt-0">
-                                            <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
+                                            <h2 class="text-xl font-semibold text-black dark:text-white">デプロイできてるか～たのむでぇ～</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
-                                                Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                                できてます。紙です。
                                             </p>
                                         </div>
                                     </div>
